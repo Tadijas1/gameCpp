87d0e289de7c3009cc41e912bdf6ae719ed258cc
Tadijas1 <ctadeusz14@gmail.com>
2026-02-19 15:12:40 +0100

    Gra na 2 graczy już działa, bez ekranu końcowego

--- pong (film2)/src/main.cpp
+++ pong (film2)/src/main.cpp
@@ -16,7 +16,7 @@ class Ball
     public:
     float x, y;
     int radius;
-    int speed_x, speed_y;
+    float speed_x, speed_y;
 
     void draw()
     {
@@ -39,11 +39,20 @@ class Ball
         x = GetScreenWidth()/2;
         y = GetScreenHeight()/2;
 
+        if(speed_x < 0) speed_x += 0.7;
+        else speed_x += 0.7;
+        if(speed_y < 0) speed_y += 0.7;
+        else speed_y += 0.7;
+
         int speed_chioces[2] = {-1, 1};
         speed_x *= speed_chioces[GetRandomValue(0,1)];
         speed_y *= speed_chioces[GetRandomValue(0,1)];
-        speed_x++;
-        speed_y++;  
+        
+        
+        // Coś tu jeszcze nie działa
+        //float direction_choise[2] = {-0.5, 0.5};
+        // speed_x += direction_choise[GetRandomValue(0,2)];
+        // speed_y += direction_choise[GetRandomValue(0,2)];
     }
 };
 
@@ -60,6 +69,7 @@ class Paddle
     float x, y;
     float width, height;
     int speed;
+    string name;
 
     void draw()
     {
@@ -75,13 +85,18 @@ class Paddle
     }
 };
 
-class CpuPddle :public Paddle
+class CpuPaddle :public Paddle
 {
     public:
-    void update(int ball_y)
+    void update(int ball_y, char w)
     {
-        if(y + height/2 > ball_y) y -= speed;
-        if(y + height/2 < ball_y) y += speed;
+        if(w == '1'){
+            if(y + height/2 > ball_y) y -= speed;
+            if(y + height/2 < ball_y) y += speed;
+        } else {
+            if(IsKeyDown(KEY_S)) y += speed;
+            if(IsKeyDown(KEY_W)) y -= speed;
+        }
 
         limitMovment();
     }
@@ -89,12 +104,30 @@ class CpuPddle :public Paddle
 
 Ball ball;
 Paddle player;
-CpuPddle cpu;
+CpuPaddle cpu;
 
 int main()
 {
     //Deklarowanie zmiennych lokalnych
     const int screen_width = 1200, screen_height = 800;
+
+    //Tryb gry
+    //Pytanie o tryb
+    bool exit = true;
+    char wybor;
+    while (exit)
+    {
+        system("cls"); //system("clear")
+        cout<<"Wybierz tryb:"<<endl;
+        cout<<"1. I gracz"<<endl;
+        cout<<"2. II graczy"<<endl;
+        wybor = getchar(); cout<<endl;
+        system("cls"); //system("clear")
+        cout<<"Wpisz nazwę gracza 1: "; cin>>player.name;
+        if(wybor == '2') {cout<<"Wpisz nazwa gracza 2: "; cin>>cpu.name;}
+        else if (wybor == '1') cpu.name = "CPU";
+        if((wybor == '1')||(wybor == '2')) exit = false;
+    }
 
     //Okno gry
     InitWindow(screen_width, screen_height, "Pong");
@@ -127,7 +160,7 @@ int main()
         //1. Update pozycji 
         ball.update();
         player.update();
-        cpu.update(ball.y);
+        cpu.update(ball.y, wybor);
 
         //2. Sprawdzanie kolozji
         if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height})) ball.speed_x *= -1;
