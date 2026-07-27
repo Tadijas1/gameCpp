4e799a78617e637ab44bfac7a319ac9f9eef9cb2
Tadijas1 <ctadeusz14@gmail.com>
2026-07-27 23:13:35 +0200

    BUTTON_TUTORIAL
    - Dodano kilka rzeczy
    - Trzeba jeszcze dorobić przyciskom hitboxy

--- buttonTutorialFilm7/src/main.cpp
+++ buttonTutorialFilm7/src/main.cpp
@@ -9,41 +9,45 @@ int main()
 {
     //zmienne lokalne
     const int windowWigth = 800;
-    const int windowHeight = 600; 
+    const int windowHeight = 600;
     int FPS = 60;
-
+    bool exit = false;
     
     //TWORZENIE OKNA
     InitWindow(windowWigth, windowHeight, "button_tutorial");
     
     //obraz w tle
     Texture2D background = LoadTexture("pictures/background.png");
+    float scale = static_cast<float>(windowHeight)/ background.height;
+
 
     //FPS
     SetTargetFPS(FPS);
 
     //TWORZENIE OBIEKTÓW
     Button startButton = Button("pictures/button_start.png", {210,20}, 0.5);
-    Button exitButton = Button("pictures/button_exit.png", {210,320}, 0.5);
+    Button exitButton = Button("pictures/button_exit.png", {210,200}, 0.5);
 
     //PĘTLA GRY
-    while(WindowShouldClose() == false)
+    while(WindowShouldClose() == false && exit == false)
     {
         //1. Kolizie i akcje czasowe
         
         
         //2. Update obiektów i urządzenia wejścia
         if(startButton.isPressed(GetMousePosition(), IsMouseButtonPressed(MOUSE_BUTTON_LEFT))) cout<<"starging game"<<endl;
-        if(exitButton.isPressed(GetMousePosition(), IsMouseButtonPressed(MOUSE_BUTTON_LEFT))) CloseWindow();
+        if(exitButton.isPressed(GetMousePosition(), IsMouseButtonPressed(MOUSE_BUTTON_LEFT))) exit = true;
 
         //3. Rysowanie następniej klatki
         BeginDrawing();
             ClearBackground(GREEN);
-            DrawTexture(background, 0, 0, WHITE);
+            DrawTextureEx(background, (Vector2){0, 0}, 0.0f, scale ,WHITE);
             startButton.Draw();
             exitButton.Draw();
         EndDrawing();
     }
+
+    UnloadTexture(background);
 
     CloseWindow();
 }
